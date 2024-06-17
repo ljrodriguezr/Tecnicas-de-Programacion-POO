15af57ea5fe12f7c89d793428032f6538523e657
LUCIA RODRIGUEZ <lj.rodriguezr@uea.edu.ec>
2024-06-16 20:17:30 -0500

    utilizacion de abstraccion, encapsulacion, herencia, polimorfismo

---
+++ semana 3/2.py
@@ -0,0 +1,140 @@
+# Crear una matriz 3D para almacenar datos de temperaturas
+# Primera dimensión: Ciudades (3 ciudades)
+# Segunda dimensión: Semanas (4 semanas)
+# Tercera dimensión: Días de la semana (7 días)
+temperaturas = [
+    [   # Ciudad 1
+        [   # Semana 1
+            {"day": "Lunes", "temp": 15},
+            {"day": "Martes", "temp": 24},
+            {"day": "Miércoles", "temp": 20},
+            {"day": "Jueves", "temp": 34},
+            {"day": "Viernes", "temp": 28},
+            {"day": "Sábado", "temp": 36},
+            {"day": "Domingo", "temp": 44}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 76},
+            {"day": "Martes", "temp": 79},
+            {"day": "Miércoles", "temp": 83},
+            {"day": "Jueves", "temp": 81},
+            {"day": "Viernes", "temp": 87},
+            {"day": "Sábado", "temp": 89},
+            {"day": "Domingo", "temp": 93}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 77},
+            {"day": "Martes", "temp": 81},
+            {"day": "Miércoles", "temp": 85},
+            {"day": "Jueves", "temp": 82},
+            {"day": "Viernes", "temp": 88},
+            {"day": "Sábado", "temp": 91},
+            {"day": "Domingo", "temp": 95}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 75},
+            {"day": "Martes", "temp": 78},
+            {"day": "Miércoles", "temp": 80},
+            {"day": "Jueves", "temp": 79},
+            {"day": "Viernes", "temp": 84},
+            {"day": "Sábado", "temp": 87},
+            {"day": "Domingo", "temp": 91}
+        ]
+    ],
+    [   # Ciudad 2
+        [   # Semana 1
+            {"day": "Lunes", "temp": 52},
+            {"day": "Martes", "temp": 54},
+            {"day": "Miércoles", "temp": 58},
+            {"day": "Jueves", "temp": 60},
+            {"day": "Viernes", "temp": 63},
+            {"day": "Sábado", "temp": 65},
+            {"day": "Domingo", "temp": 69}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 63},
+            {"day": "Martes", "temp": 66},
+            {"day": "Miércoles", "temp": 70},
+            {"day": "Jueves", "temp": 72},
+            {"day": "Viernes", "temp": 75},
+            {"day": "Sábado", "temp": 77},
+            {"day": "Domingo", "temp": 81}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 61},
+            {"day": "Martes", "temp": 65},
+            {"day": "Miércoles", "temp": 68},
+            {"day": "Jueves", "temp": 70},
+            {"day": "Viernes", "temp": 72},
+            {"day": "Sábado", "temp": 76},
+            {"day": "Domingo", "temp": 80}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 65},
+            {"day": "Martes", "temp": 68},
+            {"day": "Miércoles", "temp":70},
+            {"day": "Jueves", "temp": 72},
+            {"day": "Viernes", "temp": 73},
+            {"day": "Sábado", "temp": 78},
+            {"day": "Domingo", "temp": 81}
+        ]
+    ],
+    [   # Ciudad 3
+        [   # Semana 1
+            {"day": "Lunes", "temp": 90},
+            {"day": "Martes", "temp": 73},
+            {"day": "Miércoles", "temp": 95},
+            {"day": "Jueves", "temp": 92},
+            {"day": "Viernes", "temp": 89},
+            {"day": "Sábado", "temp": 86},
+            {"day": "Domingo", "temp": 83}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 89},
+            {"day": "Martes", "temp": 91},
+            {"day": "Miércoles", "temp": 93},
+            {"day": "Jueves", "temp": 90},
+            {"day": "Viernes", "temp": 87},
+            {"day": "Sábado", "temp": 84},
+            {"day": "Domingo", "temp": 81}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 91},
+            {"day": "Martes", "temp": 93},
+            {"day": "Miércoles", "temp": 95},
+            {"day": "Jueves", "temp": 92},
+            {"day": "Viernes", "temp": 89},
+            {"day": "Sábado", "temp": 86},
+            {"day": "Domingo", "temp": 83}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 88},
+            {"day": "Martes", "temp": 90},
+            {"day": "Miércoles", "temp": 92},
+            {"day": "Jueves", "temp": 89},
+            {"day": "Viernes", "temp": 86},
+            {"day": "Sábado", "temp": 83},
+            {"day": "Domingo", "temp": 80}
+        ]
+    ]
+]
+def calcular_promedio(suma_acumulada, total_acumulado):
+    return round(suma_acumulada / total_acumulado, 2)
+
+# Calcular el promedio de temperaturas para cada ciudad y semana
+no_ciudad = 0
+for ciudad in temperaturas:
+    no_ciudad = no_ciudad + 1
+    print(f'CIUDAD No. {no_ciudad}')
+    no_semana = 0
+    suma_promedio = 0
+    for semana in ciudad:
+        no_semana += 1
+        suma = 0
+        for dia in semana:
+            suma = suma + dia['temp']
+        promedio = calcular_promedio(suma, len(semana))
+        suma_promedio += promedio
+        print(f'El promedio semana No. {no_semana} es: {promedio}')
+    promedio_ciudad = calcular_promedio(suma_promedio, len(ciudad) )
+    print(f'El promedio mensual es: {promedio_ciudad}')
